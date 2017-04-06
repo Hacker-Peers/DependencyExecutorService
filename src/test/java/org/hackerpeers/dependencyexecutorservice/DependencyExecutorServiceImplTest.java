43b71d2d3146c82a36d791cbec3a1f7cb5a53b80
Simon Berthiaume <sberthiaume@gmail.com>
2017-04-05 23:08:53 -0400

    Adding a few tests for delegated methods.

--- src/test/java/org/hackerpeers/dependencyexecutorservice/DependencyExecutorServiceImplTest.java
+++ src/test/java/org/hackerpeers/dependencyexecutorservice/DependencyExecutorServiceImplTest.java
@@ -7,10 +7,12 @@ import org.testng.annotations.Test;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsSame.sameInstance;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -54,5 +56,44 @@ public class DependencyExecutorServiceImplTest {
         // Then
         verify(mockExecutorService, times(1)).shutdownNow();
         assertThat(actual, is(sameInstance(expected)));
+    }
+
+    @Test
+    public void isShutdownCallsDelegateAndReturnSameValue() {
+        // Given
+        doReturn(false).when(mockExecutorService).isShutdown();
+
+        // When
+        boolean actual = subject.isShutdown();
+
+        // Then
+        verify(mockExecutorService, times(1)).isShutdown();
+        assertThat(actual, is(false));
+    }
+
+    @Test
+    public void isTerminatedCallsDelegateAndReturnSameValue() {
+        // Given
+        doReturn(false).when(mockExecutorService).isTerminated();
+
+        // When
+        boolean actual = subject.isTerminated();
+
+        // Then
+        verify(mockExecutorService, times(1)).isTerminated();
+        assertThat(actual, is(false));
+    }
+
+    @Test
+    public void awaitTerminationCallsDelegateWithProperParametersAndReturnSameValue() throws InterruptedException {
+        // Given
+        doReturn(false).when(mockExecutorService).awaitTermination(any(Long.class), any(TimeUnit.class));
+
+        // When
+        boolean actual = subject.awaitTermination(12345, TimeUnit.MINUTES);
+
+        // Then
+        verify(mockExecutorService, times(1)).awaitTermination(12345, TimeUnit.MINUTES);
+        assertThat(actual, is(false));
     }
 }
