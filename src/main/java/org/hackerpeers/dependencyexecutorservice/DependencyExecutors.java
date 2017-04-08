b32ffc4f33b7afa918624e93f87dd2108f555e85
Simon Berthiaume <sberthiaume@gmail.com>
2017-04-08 15:33:53 -0400

    Setting DependencyExecutors' constructor as private as it's meant to be a utility class.

--- src/main/java/org/hackerpeers/dependencyexecutorservice/DependencyExecutors.java
+++ src/main/java/org/hackerpeers/dependencyexecutorservice/DependencyExecutors.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ExecutorService;
  * @author @sberthiaume
  */
 public class DependencyExecutors {
+    private DependencyExecutors(){}
+
     public static DependencyExecutorService newDependencyExecutorWithDelegate(ExecutorService delegate) {
         return new DependencyExecutorServiceImpl(delegate);
     }
